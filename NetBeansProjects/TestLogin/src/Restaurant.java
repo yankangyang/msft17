db7aaeebb5a0fac349292209b7b33317d0dd091f
yankangyang <yankangyang@gmail.com>
2017-05-30 17:26:13 -0700

    Graph DB Connected

---
+++ NetBeansProjects/TestLogin/src/Restaurant.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+class Restaurant {
+    // declare variables are private 
+    private final int rating;
+    private final String name;
+    
+    // public function calls
+    public Restaurant(String name, int rating){
+        this.rating = rating;
+        this.name = name;
+       
+    }
+    
+    public int getrating(){
+        return rating;
+    }
+    
+    public String getname(){
+        return name;
+    }
+}
